2dbd7273b6aab92f4deef9eab027f8012690b69c
Oscar <oscarwasil@gmail.com>
2018-08-30 20:19:30 -0500

    Version 2

--- v1.py
+++ v1.py
@@ -1,27 +1,32 @@
-print("Rock...")
-print("Paper...")
-print("Scissors...")
+from random import randint
 
-player1 = input("Player 1 make your choice ")
-print("********************No Cheating\n\n" * 15)
-player2 = input("Player 2 make your choice ")
+player = input("Player make your choice: ").lower()
 
-if player1 == player2:
-  print("It's a tie!")
-elif player1 == "rock":
-    if player2 == "scissors":
-      print("player1 wins!")
-    elif player2 == "paper":
-      print("player2 wins!")
-elif player1 == "paper":
-    if player2 == "rock":
-      print("player1 wins!")
-    elif player2 == "scissors":
-      print("player2 wins!")
-elif player1 == "scissors":
-    if player2 == "paper":
-      print("player1 wins!")
-    elif player2 == "rock":
-      print("player2 wins!")
+rand_num = randint(0,2)
+if rand_num == 0:
+    computer = "rock"
+elif rand_num == 1:
+    computer = "paper" 
 else:
-    print("Something went wrong.")
+    computer = "scissors"
+print(f"Computer plays {computer}")
+
+if player == computer:
+  print("It's a tie!")
+elif player == "rock":
+    if computer == "scissors":
+      print("player wins!")
+    else:
+      print("computer wins!")
+elif player == "paper":
+    if computer == "rock":
+      print("player wins!")
+    elif computer == "scissors":
+      print("computer wins!")
+elif player == "scissors":
+    if computer == "paper":
+      print("player wins!")
+    elif computer == "rock":
+      print("computer wins!")
+else:
+    print("Please enter a valid move")
